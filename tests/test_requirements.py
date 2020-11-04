12b8e5419154e730d5b0558758a8759e8c0408f0
raman325 <7243222+raman325@users.noreply.github.com>
2020-11-04 15:28:36 -0500

    add additional tests

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -164,6 +164,8 @@ async def test_install_unpinned_requirements(hass, caplog):
         entry = MockConfigEntry(domain=DOMAIN, data={CONF_ALLOW_ALL_IMPORTS: True})
         entry.add_to_hass(hass)
 
+        # Check that unpinned version gets skipped because a version is already
+        # installed
         process_requirements.return_value = {
             "my-package-name": {
                 ATTR_SOURCES: [
@@ -178,6 +180,8 @@ async def test_install_unpinned_requirements(hass, caplog):
         await hass.async_block_till_done()
         assert not ha_install_requirements.called
 
+        # Check that unpinned version gets installed because it isn't already
+        # installed
         process_requirements.return_value = {
             "my-package-name": {
                 ATTR_SOURCES: [
@@ -200,7 +204,96 @@ async def test_install_unpinned_requirements(hass, caplog):
         await hass.async_block_till_done()
         assert ha_install_requirements.called
         assert ha_install_requirements.call_args[0][2] == ["my-package-name", "my-package-name-1==2.0.1"]
+        # my-package-name will show as not installed and therefore won't be included
         assert entry.data[CONF_INSTALLED_PACKAGES] == {"my-package-name-1": "2.0.1"}
+
+        # Check that entry.data[CONF_INSTALLED_PACKAGES] gets updated with a version number
+        # when unpinned version was requested
+        with patch("custom_components.pyscript.requirements.installed_version", return_value="1.1.1"):
+            process_requirements.return_value = {
+                "my-package-name": {
+                    ATTR_SOURCES: [
+                        f"{PYSCRIPT_FOLDER}/requirements.txt",
+                        f"{PYSCRIPT_FOLDER}/apps/app1/requirements.txt",
+                    ],
+                    ATTR_VERSION: UNPINNED_VERSION,
+                    ATTR_INSTALLED_VERSION: None,
+                },
+                "my-package-name-1": {
+                    ATTR_SOURCES: [
+                        f"{PYSCRIPT_FOLDER}/requirements.txt",
+                        f"{PYSCRIPT_FOLDER}/apps/app1/requirements.txt",
+                    ],
+                    ATTR_VERSION: "2.0.1",
+                    ATTR_INSTALLED_VERSION: None,
+                },
+            }
+            await install_requirements(hass, entry, PYSCRIPT_FOLDER)
+            await hass.async_block_till_done()
+            assert ha_install_requirements.called
+            assert ha_install_requirements.call_args[0][2] == ["my-package-name", "my-package-name-1==2.0.1"]
+            assert entry.data[CONF_INSTALLED_PACKAGES] == {
+                "my-package-name": "1.1.1",
+                "my-package-name-1": "2.0.1",
+            }
+
+        # Check that package gets removed from entry.data[CONF_INSTALLED_PACKAGES] when it was
+        # previously installed by pyscript but version was changed presumably by another system
+        process_requirements.return_value = {
+            "my-package-name": {
+                ATTR_SOURCES: [
+                    f"{PYSCRIPT_FOLDER}/requirements.txt",
+                    f"{PYSCRIPT_FOLDER}/apps/app1/requirements.txt",
+                ],
+                ATTR_VERSION: UNPINNED_VERSION,
+                ATTR_INSTALLED_VERSION: "2.0.0",
+            },
+            "my-package-name-1": {
+                ATTR_SOURCES: [
+                    f"{PYSCRIPT_FOLDER}/requirements.txt",
+                    f"{PYSCRIPT_FOLDER}/apps/app1/requirements.txt",
+                ],
+                ATTR_VERSION: "2.0.1",
+                ATTR_INSTALLED_VERSION: None,
+            },
+        }
+        await install_requirements(hass, entry, PYSCRIPT_FOLDER)
+        await hass.async_block_till_done()
+        assert ha_install_requirements.called
+        assert ha_install_requirements.call_args[0][2] == ["my-package-name-1==2.0.1"]
+        assert entry.data[CONF_INSTALLED_PACKAGES] == {"my-package-name-1": "2.0.1"}
+
+
+async def test_install_requirements_not_allowed(hass):
+    """Test that install requirements will not work because 'allow_all_imports' is False."""
+    with patch(
+        "custom_components.pyscript.requirements.process_all_requirements"
+    ) as process_requirements, patch(
+        "custom_components.pyscript.requirements.async_process_requirements"
+    ) as ha_install_requirements:
+        entry = MockConfigEntry(domain=DOMAIN, data={CONF_ALLOW_ALL_IMPORTS: False})
+        entry.add_to_hass(hass)
+
+        # Check that packages get installed correctly
+        process_requirements.return_value = {
+            "my-package-name": {
+                ATTR_SOURCES: [
+                    f"{PYSCRIPT_FOLDER}/requirements.txt",
+                    f"{PYSCRIPT_FOLDER}/apps/app1/requirements.txt",
+                ],
+                ATTR_VERSION: "2.0.1",
+                ATTR_INSTALLED_VERSION: None,
+            },
+            "my-package-name-alternate": {
+                ATTR_SOURCES: [f"{PYSCRIPT_FOLDER}/requirements.txt"],
+                ATTR_VERSION: "2.0.1",
+                ATTR_INSTALLED_VERSION: None,
+            },
+        }
+        assert await install_requirements(hass, entry, PYSCRIPT_FOLDER) is None
+        await hass.async_block_till_done()
+
+        assert not ha_install_requirements.called
 
 
 def test_process_requirements():
