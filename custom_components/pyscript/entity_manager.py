3042aa190253250f33485e0707d4f34664d68431
Daniel Lashua <daniel@inklog.net>
2020-12-28 07:32:23 -0600

    PyscriptEntity base class

--- custom_components/pyscript/entity_manager.py
+++ custom_components/pyscript/entity_manager.py
@@ -1,3 +1,10 @@
+from homeassistant.helpers.entity import Entity
+from .const import DOMAIN, LOGGER_PATH
+import logging
+
+_LOGGER = logging.getLogger(LOGGER_PATH + ".entity_manager")
+
+
 class EntityManager:
 
     hass = None
@@ -30,3 +37,73 @@ class EntityManager:
         cls.platform_adders[platform]([new_entity])
         cls.registered_entities[platform][name] = new_entity
         
+
+class PyscriptEntity(Entity):
+
+    def __init__(self, hass, unique_id):
+        self._added = False
+        self.hass = hass
+
+        self._unique_id = f"{DOMAIN}_{self.platform}_{unique_id}"
+
+        self._state = None
+        self._attributes = {}
+        self._icon = None
+
+
+        _LOGGER.debug(
+            "Entity Initialized %s",
+            self._unique_id,
+        )
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def device_state_attributes(self):
+        """Return attributes for the sensor."""
+        return self._attributes
+
+    @property
+    def icon(self):
+        """Return the icon to use in the frontend, if any."""
+        return self._icon
+
+    @property
+    def unique_id(self):
+        """Return a unique ID."""
+        return self._unique_id
+
+    async def async_added_to_hass(self):
+        self._added = True
+        await self.async_update()
+
+    async def async_update(self):
+        self.async_write_ha_state()
+
+
+    # TO BE USED IN PYSCRIPT
+    ######################################
+
+    async def set(self, state=None, new_attributes=None, **kwargs):
+        if state is not None:
+            self._state = state
+
+        if new_attributes is not None:
+            self._attributes = new_attributes
+
+        for attribute_name in kwargs:
+            self._attributes[attribute_name] = kwargs[attribute_name]
+
+
+        _LOGGER.debug(
+            "%s state is now %s (%s)",
+            self._unique_id,
+            self._state,
+            self._attributes,
+        )
+
+        if self._added:
+            await self.async_update()
--- custom_components/pyscript/sensor.py
+++ custom_components/pyscript/sensor.py
@@ -1,6 +1,7 @@
-from homeassistant.helpers.entity import Entity
 from .const import DOMAIN
-from .entity_manager import EntityManager
+from .entity_manager import EntityManager, PyscriptEntity
+
+PLATFORM = 'sensor'
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     EntityManager.register_platform('sensor', async_add_entities, PyscriptSensor)
@@ -11,52 +12,6 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     )
 
 
-class PyscriptSensor(Entity):
-
-    def __init__(self, hass, name):
-        self._added = False
-        self.hass = hass
-
-        self._name = name
-        self._state = None
-        self._attributes = {}
-
-        self._unique_id = f"{DOMAIN}_sensor_{self._name}"
-
-    @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def device_state_attributes(self):
-        """Return attributes for the sensor."""
-        return self._attributes
-
-    @property
-    def icon(self):
-        """Return the icon to use in the frontend, if any."""
-        return "mdi:home"
-
-    @property
-    def unique_id(self):
-        """Return a unique ID."""
-        return self._unique_id
-
-    async def async_added_to_hass(self):
-        self._added = True
-        await self.async_update()
-
-    async def set_state(self, state):
-        self._state = state
-        if self._added:
-            await self.async_update()
-
-    async def async_update(self):
-        self.async_write_ha_state()
+class PyscriptSensor(PyscriptEntity):
+    platform = PLATFORM
 
