12e3e3f5fa29316eaf935685a913d4617833f4e8
Daniel Lashua <daniel@inklog.net>
2022-03-04 23:39:18 +0000

    happy tests

--- custom_components/pyscript/entity.py
+++ custom_components/pyscript/entity.py
@@ -1,9 +1,14 @@
+"""Entity Classes"""
 from homeassistant.helpers.restore_state import RestoreEntity
 
 
 class PyscriptEntity(RestoreEntity):
+    """Generic Pyscript Entity"""
+
     def set_state(self, state):
+        """Set the state"""
         self._attr_state = state
 
     def set_attributes(self, attributes):
+        """Set Attributes"""
         self._attr_extra_state_attributes = attributes
