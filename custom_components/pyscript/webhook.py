3d435e04c39884df8cfaf5138dbc9c8d7323b0d6
Diego Grassato <diego.grassato@bradesco.com.br>
2026-07-16 13:35:12 -0300

    fix(webhook): restore aiohttp request in legacy webhook func_args

--- custom_components/pyscript/webhook.py
+++ custom_components/pyscript/webhook.py
@@ -42,6 +42,7 @@ class Webhook:
         func_args = {
             "trigger_type": "webhook",
             "webhook_id": webhook_id,
+            "request": request,
         }
 
         if "json" in request.headers.get(hdrs.CONTENT_TYPE, ""):
